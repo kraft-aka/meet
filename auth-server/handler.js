3d00f7b679cbc5ad0b2439b51036a4de71df33bb
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-06-20 23:37:16 +0200

    define getCalendarEvents

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -32,6 +32,7 @@ module.exports.getAuthURL = async () => {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
     },
     body: JSON.stringify({
       authUrl: authUrl,
@@ -48,7 +49,7 @@ module.exports.getAccessToken = async (event) => {
 
   // decode authorization code extracted from thr URL query
   const code = decodeURIComponent(`${event.pathParameters.code}`);
-    console.log('-------------',code)
+  console.log("-------------", code);
   return new Promise((resolve, reject) => {
     oAuth2Client.getToken(code, (err, token) => {
       if (err) {
@@ -63,7 +64,7 @@ module.exports.getAccessToken = async (event) => {
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
-          'Access-Control-Allow-Credentials': true,
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(token),
       };
@@ -79,3 +80,52 @@ module.exports.getAccessToken = async (event) => {
 };
 
 
+module.exports.getCalendarEvents = event => {
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+
+  oAuth2Client.setCredentials({ access_token })
+
+  return new Promise((resolve, reject) => {
+    
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
+};
