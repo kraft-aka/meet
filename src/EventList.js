77107d5f1b1e341e3ee0887f028efa725ac8dd36
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-06-21 21:17:24 +0200

    test filter events

---
+++ src/EventList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Event from "./Event";
+
+class EventList extends Component {
+  render() {
+    const { events } = this.props;
+    <ul className="EventList">
+      { events.map(event => 
+        <li key={event.id}>
+          <Event event={event} />
+        </li>)}
+    </ul>
+  }
+}
+
+export default EventList;
