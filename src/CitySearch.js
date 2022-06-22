a67d2ce61529e997785c983bfb9f135c33695493
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-06-22 15:26:51 +0200

    citysearch test

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,24 +1,49 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
-  constructor() {
-    super();
+  state = {
+    query: "",
+    suggestions: [],
+  };
 
-    this.state = {
-      query: '',
-    }
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
+
+  handleItemClicked = (suggestion)=> {
+    this.setState({
+      query: suggestion
+    });
   }
-  
+
   render() {
     return (
       <div className="CitySearch">
-        <input type="text" className="city" value={this.state.query}/>
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
         <ul className="suggestions">
-
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}
+            onClick={()=> this.handleItemClicked(suggestion)}
+            >{suggestion}</li>
+          ))}
+          <li key="all">
+            <b>See all Cities</b>
+          </li>
         </ul>
-
       </div>
-    )
+    );
   }
 }
 
