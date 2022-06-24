a35bb756353268c7a7606d750b8be420d9ade728
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-06-24 15:37:28 +0200

    integration test for app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents />
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} />
+        <EventList events={this.state.events}/>
+        <NumberOfEvents />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "../App";
 import EventList from '../EventList';
 import CitySearch from "../CitySearch";
 import NumberOfEvents from "../NumberOfEvents";
+
+// scope for unit testing
 
 describe('<App /> component',()=> {
   let AppWrapper;
@@ -24,3 +26,24 @@ describe('<App /> component',()=> {
   })
 
 });
+
+// scope for integration testing
+
+describe('<App /> integration',()=> {
+
+  test('App passes "events" state as a prop to EventList', ()=> {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
+  })
+
+  test('App passes "locations" state as a prop to CitySearch',()=> {
+    const AppWrapper = mount(<App />);
+    const AppLocationsState = AppWrapper.state('locations');
+    expect(AppLocationsState).not.toEqual(undefined);
+    expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+    AppWrapper.unmount();
+  })
+})
