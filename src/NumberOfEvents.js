7eef2d3f3f763c766d80f3217ef790ddc37430d3
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-07-05 22:41:02 +0200

    code refactor

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -4,23 +4,31 @@ import { ErrorAlert } from "./Alert";
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
-    infoText:'',
+    infoText: "",
   };
 
   handleEventNumberChanged = (e) => {
     const newNumberOfEvent = e.target.value;
 
-    if (newNumberOfEvent <= 0 || newNumberOfEvent > 32 ||typeof newNumberOfEvent === "number") {
-      this.setState({ numberOfEvents: newNumberOfEvent, infoText: 'Select number from 1 to 32' });
+    if (
+      newNumberOfEvent <= 0 ||
+      newNumberOfEvent > 32 ||
+      typeof newNumberOfEvent === "number"
+    ) {
+      this.setState({
+        numberOfEvents: newNumberOfEvent,
+        infoText: "Select number from 1 to 32",
+      });
     } else if (newNumberOfEvent > 0 && newNumberOfEvent < 32) {
-      this.setState({ numberOfEvents: newNumberOfEvent, infoText: ''});
-    } 
+      this.setState({ numberOfEvents: newNumberOfEvent, infoText: "" });
+    }
     this.props.updateEvents(undefined, newNumberOfEvent);
   };
 
   render() {
     return (
       <div className="number-box">
+        <ErrorAlert text={this.state.infoText} />
         <label className="number-events--label">Number of Events</label>
         <input
           type="text"
@@ -28,7 +36,6 @@ class NumberOfEvents extends Component {
           value={this.state.numberOfEvents}
           onChange={this.handleEventNumberChanged}
         />
-        <ErrorAlert text={this.state.infoText}/>
       </div>
     );
   }
