cbf0c40a7650fe81c2c8f5043d6e7bc79bb5b9e3
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-07-05 19:48:32 +0200

    init error alert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
+    infoText:'',
   };
 
   handleEventNumberChanged = (e) => {
     const newNumberOfEvent = e.target.value;
 
-    if (newNumberOfEvent <= 0 || typeof newNumberOfEvent === "number") {
-      this.setState({ numberOfEvents: newNumberOfEvent });
+    if (newNumberOfEvent <= 0 || newNumberOfEvent > 32 ||typeof newNumberOfEvent === "number") {
+      this.setState({ numberOfEvents: newNumberOfEvent, infoText: 'Select number from 1 to 32' });
     } else if (newNumberOfEvent > 0 && newNumberOfEvent < 32) {
-      this.setState({ numberOfEvents: newNumberOfEvent });
-    }
+      this.setState({ numberOfEvents: newNumberOfEvent, infoText: ''});
+    } 
     this.props.updateEvents(undefined, newNumberOfEvent);
   };
 
   render() {
-    const { events, updateEvents } = this.props;
     return (
       <div className="number-box">
         <label className="number-events--label">Number of Events</label>
@@ -27,6 +28,7 @@ class NumberOfEvents extends Component {
           value={this.state.numberOfEvents}
           onChange={this.handleEventNumberChanged}
         />
+        <ErrorAlert text={this.state.infoText}/>
       </div>
     );
   }
