2d83da34b9c8d5ea140ec330e4791e58c3843846
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-07-13 17:30:36 +0200

    code refactor

--- src/api.js
+++ src/api.js
@@ -11,12 +11,14 @@ export const extractLocations = (events) => {
 
 //check the token and its validity
 export const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-  return result;
+  try {
+    const result = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+    return await result.json();
+  } catch (error) {
+    error.json();
+  }
 };
 
 
