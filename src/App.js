11ced41306a3973c06ef1bb12cb8a97ca26f60df
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-07-16 12:15:43 +0200

    init getData

--- src/App.js
+++ src/App.js
@@ -43,6 +43,16 @@ class App extends Component {
     this.updateEvents(this.state.locations, numberOfEvents);
   };
 
+  getData= () => {
+    const {locations, events} =this.state;
+    const data = locations.map((location)=> {
+      const number = events.filter((event)=> event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  }
+
   async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem("access_token");
