47970697ae3e5a7ef03d7c1941710ace3258b8a6
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-07-16 12:40:37 +0200

    update scatterChart

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
 
 class App extends Component {
@@ -91,7 +92,7 @@ class App extends Component {
   }
 
   render() {
-    const {locations, numberOfEvents } = this.state;
+    const { locations, numberOfEvents } = this.state;
     if (this.state.showWelcomeScreen === undefined)
       return <div className="App" />;
 
@@ -107,17 +108,20 @@ class App extends Component {
           events={this.state.events}
         />
 
-        <ScatterChart
-          width={730}
-          height={250}
-          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
-        >
-          <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city" />
-          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Scatter data={this.getData()} fill="#8884d8" />
-        </ScatterChart>
+        <ResponsiveContainer height={400}>
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              type="number"
+              dataKey="number"
+              name="number of events"
+              allowDecimals={false}
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
 
         <EventList events={this.state.events} />
         <WelcomeScreen
